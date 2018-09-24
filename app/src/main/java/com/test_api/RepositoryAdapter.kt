2e05d7c0695c9c09d25d1f4cbfb88a896cc8363b
Gabriel Esteves <gabriel.esteves@kanamobi.com.br>
2018-09-24 18:52:51 -0300

    Three Commit
    Three Commit

--- app/src/main/java/com/test_api/RepositoryAdapter.kt
+++ app/src/main/java/com/test_api/RepositoryAdapter.kt
@@ -2,5 +2,5 @@ package com.test_api
 
 import android.widget.Adapter
 
-class RepositoryAdapater {
+class RepositoryAdapter {
 }
---
+++ app/src/main/java/com/test_api/RetrofitInitializer.kt
@@ -0,0 +1,10 @@
+package com.test_api
+
+import retrofit2.Retrofit
+
+class RetrofitInitializer {
+    fun init() {
+        Retrofit.Builder().baseUrl()
+
+    }
+}
