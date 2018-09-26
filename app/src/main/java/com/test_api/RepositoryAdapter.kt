0abe17e607bcf7ca4745621dc8160d007819a217
Gabriel Esteves <gabriel.esteves@kanamobi.com.br>
2018-09-26 19:11:14 -0300

    commit
    commit

--- app/src/main/java/com/test_api/RepositoryAdapter.kt
+++ app/src/main/java/com/test_api/RepositoryAdapter.kt
@@ -3,32 +3,46 @@ package com.test_api
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import com.test_api.RepositoryAdapter.ViewHolder
+import kotlinx.android.synthetic.main.item_list_repositories.view.*
 
 
 class RepositoryAdapter(private val repositories: List<Repository>,
                         private val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val repository = repositories[position]
+        holder?.let {
+            it.bindView(repository)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_list_repositories, parent, false)
         return RecyclerView.ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val repository = repositories[position]
-        holder?.let {
-            it.textNameRepository = repository.nameRepo
-            it.
-            it.textDescriptionRepository = repository.descriptionRepo
-            it.imageUser  = repository
-            it.textUsernameRepository = repository.imageUser
-            it.extSecondNameRepository = repository.subnameUser
-        }
-    }
-
     override fun getItemCount(): Int {
         return repositories.size
+    }
+
+    class RepositoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindView(repository: Repository) {
+
+
+            val nameRepo = itemView.textNameRepository
+            val descriptionRepo = itemView.textDescriptionRepository
+            val userName = itemView.textUsernameRepository
+            val secondNameUser = itemView.textSecondNameRepository
+
+
+            nameRepo.text = repository.nameRepo
+            descriptionRepo.text = repository.descriptionRepo
+            userName.text = repository.nameUser
+            secondNameUser.text = repository.subnameUser
+        }
     }
 }
 
--- app/src/main/java/com/test_api/RepositoryViewHolder.kt
+++ app/src/main/java/com/test_api/RepositoryViewHolder.kt
@@ -7,8 +7,18 @@ import kotlinx.android.synthetic.main.item_list_repositories.view.*
 class RepositoryViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bindView(repository: Repository) {
-        val  = itemView.textNameRepository
-                val
+        val nameRepo = itemView.textNameRepository
+        val descriptionRepo = itemView.textDescriptionRepository
+        val userName = itemView.textUsernameRepository
+        val secondNameUser = itemView.textSecondNameRepository
+
+
+        nameRepo.text = repository.nameRepo
+        descriptionRepo.text = repository.descriptionRepo
+        userName.text = repository.nameUser
+        secondNameUser.text = repository.subnameUser
     }
 
 }
+
+
